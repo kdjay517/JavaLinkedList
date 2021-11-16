287fec965bb582437136d56e9b96a9c0d831a678
kdjay517 <kdjay517@gmail.com>
2021-11-16 20:53:02 +0530

    added Insertion and showing stored data in linked list

---
+++ src/com/bridgelabz/linkedlist/LinkedListInterface.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.linkedlist;
+
+public interface LinkedListInterface {
+	
+	public void Insert(int data);
+	public void Show();
+}
---
+++ src/com/bridgelabz/linkedlist/Runner.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.linkedlist;
+
+import java.util.Scanner;
+
+public class Runner {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1 to add the data at the start");
+        switch(scanner.nextInt()) {
+            case 1:
+                Operations.addDataAtStart();
+                scanner.close();
+        }
+		
+	
+	}
+
+}
