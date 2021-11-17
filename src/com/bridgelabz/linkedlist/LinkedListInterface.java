a7f3c68746271025fb88ee9ca2fd8e62b099216a
kdjay517 <kdjay517@gmail.com>
2021-11-17 07:01:24 +0530

    added deleting first node in linked list

---
+++ src/com/bridgelabz/linkedlist/LinkedListInterface.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.linkedlist;
+
+public interface LinkedListInterface {
+	
+	public Node Insert(int data);
+	public void Append(int data);
+	public void Show();
+	public void insertAt(int index,int data);
+	public Node pop();
+}
---
+++ src/com/bridgelabz/linkedlist/Runner.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.linkedlist;
+
+import java.util.Scanner;
+
+public class Runner {
+	public static void main(String[] args) {
+		Operations operation = new Operations();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter 1 to add the data at the start\n" + "Enter 2 to add the data at the end\n"
+				+ "Enter 3 to add the data at middle\n" + "Enter 4 to delete the first element\n");
+		switch (scanner.nextInt()) {
+		case 1:
+			operation.addDataAtStart();
+			break;
+		case 2:
+			operation.addDataAtEnd();
+			break;
+		case 3:
+			operation.addDataAtMiddle();
+			break;
+		case 4:
+			operation.deleteFirstNode();
+			break;
+		}
+		scanner.close();
+	}
+
+}
